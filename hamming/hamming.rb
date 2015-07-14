177e9e04624f402b090361dc0c9856031b9f1f2a
Bret Doucette <doucette.bret@gmail.com>
2015-07-14 12:06:25 -0600

    Hamming V.1

--- hamming/hamming.rb
+++ hamming/hamming.rb
@@ -4,7 +4,6 @@ class Hamming
 	def self.compute(strand_1,strand_2)
 
 		raise ArgumentError, "The two strands must have the same length." if strand_1.length != strand_2.length
-
 		strand_1 = strand_1.split("")
 		strand_2 = strand_2.split("")
 		
@@ -18,7 +17,6 @@ class Hamming
 			end 
 			character += 1
 			
-
 		end
 		results.count	 
 	end   
