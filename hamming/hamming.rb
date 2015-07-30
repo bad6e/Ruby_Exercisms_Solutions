47b8705ba3420761d8ffb162002fbfdb8179427f
Bret Doucette <doucette.bret@gmail.com>
2015-07-30 12:27:45 -0600

    Stuff

--- hamming/hamming.rb
+++ hamming/hamming.rb
@@ -1,18 +1,18 @@
 class Hamming
-	VERSION = 3
+  VERSION = 3
 
-	def self.compute(strand_1, strand_2)
+  def self.compute(strand_1, strand_2)
 
-		raise ArgumentError, "The two strands must have the same length." if strand_1.length != strand_2.length
+    raise ArgumentError, "The two strands must have the same length." if strand_1.length != strand_2.length
 
-		strand_1 = strand_1.split("")
-		strand_2 = strand_2.split("")
+    strand_1 = strand_1.split("")
+    strand_2 = strand_2.split("")
 
-		strand = strand_1.select.with_index do |letter, index|
-			letter != strand_2[index]
-		end
-			strand.count
-	end
+    strand = strand_1.select.with_index do |letter, index|
+      letter != strand_2[index]
+    end
+      strand.count
+  end
 end
 
 Hamming.compute('GGACTGA', 'GGACTGA').inspect
