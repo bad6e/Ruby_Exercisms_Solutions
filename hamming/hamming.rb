5d6a1b66585dbf2423ae57f6f25aea4d2dc93b2a
Bret Doucette <doucette.bret@gmail.com>
2015-07-30 15:13:05 -0600

    Cleaned up code using Rubocop

--- hamming/hamming.rb
+++ hamming/hamming.rb
@@ -2,21 +2,15 @@ class Hamming
   VERSION = 3
 
   def self.compute(strand_1, strand_2)
-
     raise ArgumentError, "The two strands must have the same length." if strand_1.length != strand_2.length
-
-    strand_1 = strand_1.split("")
-    strand_2 = strand_2.split("")
-
+    strand_1 = strand_1.split('')
+    strand_2 = strand_2.split('')
     strand = strand_1.select.with_index do |letter, index|
       letter != strand_2[index]
     end
       strand.count
   end
 end
-
-Hamming.compute('GGACTGA', 'GGACTGA').inspect
-
 
 
 
