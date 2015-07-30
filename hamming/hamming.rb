60d7e17e2f8f0449c10e23259d91065aa31be236
Bret Doucette <doucette.bret@gmail.com>
2015-07-30 11:55:05 -0600

    Added Enum Methods

--- hamming/hamming.rb
+++ hamming/hamming.rb
@@ -1,30 +1,21 @@
 class Hamming
-	VERSION = 2
+	VERSION = 3
 
-	def self.compute(strand_1,strand_2)
+	def self.compute(strand_1, strand_2)
 
 		raise ArgumentError, "The two strands must have the same length." if strand_1.length != strand_2.length
+
 		strand_1 = strand_1.split("")
 		strand_2 = strand_2.split("")
-		
-		character = 0
-		results = []
 
-		strand = strand_1.select do |letter|
-			result = letter != strand_2[character]
-			if result != false
-				results << letter
-			end 
-			character += 1
-			
+		strand = strand_1.select.with_index do |letter, index|
+			letter != strand_2[index]
 		end
-		results.count	 
-	end   
-end 
+			strand.count
+	end
+end
 
-Hamming.compute('A', 'A')
-
-
+Hamming.compute('GGACTGA', 'GGACTGA').inspect
 
 
 
