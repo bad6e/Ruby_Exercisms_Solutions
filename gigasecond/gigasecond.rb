2778b5c725096b25f137e213de7a3d4a81bb0784
Bret Doucette <doucette.bret@gmail.com>
2015-07-30 15:22:39 -0600

    Cleaned up code - set variables

--- gigasecond/gigasecond.rb
+++ gigasecond/gigasecond.rb
@@ -1,24 +1,13 @@
-# Write a program that will calculate the date that someone turned or will celebrate their 1 Gs anniversary.
-
-# A gigasecond is one billion (10**9) seconds.
 class Gigasecond
-	VERSION = 1
+  VERSION = 2
 
-	def self.from(time)
-		tnow = time 
-		tgig = tnow + (1000000000)
-		puts tgig
-	end 
+  def self.from(time)
+    gigasecond = 1000000000
+    time_now = time
+    time_gigaecond = time_now + gigasecond
+  end
+end
 
-end  
-
-Gigasecond.from(Time.utc(1985, 11, 18, 1, 0, 0))
-
-
-#Just playing around with time stuff
-# current_time = Time.new
-# formatted_time = current_time.strftime("%A %B %d %Y at %I:%M%p and %S seconds.")
-# puts "The current date and time is #{formatted_time}"
 
 
 
