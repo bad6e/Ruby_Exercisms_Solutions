6a898ee1c9e69606498ee47b044fe061c0732310
Bret Doucette <doucette.bret@gmail.com>
2015-08-02 15:58:25 -0600

    Refactored Code

--- rna-transcription/rna_transcription.rb
+++ rna-transcription/rna_transcription.rb
@@ -1,95 +1,26 @@
-#Write a program that, given a DNA strand, returns its RNA complement (per RNA transcription).
+class Complement
+  VERSION = 2
+  def self.of_dna(strand)
+    incoming_strand = strand.split("")
+    raise ArgumentError, "WRONG" if incoming_strand == ["U"]
+    rna_strand = []
+    incoming_strand.map do |word|
+      rna_strand << key[word]
+    end
+    rna_strand.join("")
+  end
 
-# * `G` -> `C`
-# * `C` -> `G`
-# * `T` -> `A`
-# * `A` -> `U`
+  def self.of_rna(strand)
+    incoming_strand = strand.split("")
+    raise ArgumentError, "WRONG" if incoming_strand == ["T"]
+    dna_strand = []
+    incoming_strand.map do |word|
+      dna_strand << key.invert[word]
+    end
+    dna_strand.join("")
+  end
 
-# Complement.of_dna('ACGTGGTCTTAA')
-# assert_equal 'UGCACCAGAAUU'
-
-
-class Complement 
-	VERSION = 1
-	def self.of_dna(strand)
-
-		firststrand = strand.split("")
-		raise ArgumentError, "WRONG" if firststrand == ["U"]
-
-		i = 0 
-		secondstrand = []
-
-		firststrand.each do |s|
-			if firststrand[i] == "A"
-				secondstrand.push("U")
-			elsif firststrand[i] == "C"
-				secondstrand.push("G")
-			elsif firststrand[i] == "T"
-				secondstrand.push("A")
-			elsif firststrand[i] == "G"
-				secondstrand.push("C")
-			else
-				puts "No"
-			end 
-		i += 1
-		end
-	secondstrand.map { |i| "" + i.to_s + "" }.join("")
-
-
-	end
-
-	def self.of_rna(strand)
-		firststrand = strand.split("")
-		raise ArgumentError, "WRONG" if firststrand == ["T"]
-
-
-		i = 0 
-		secondstrand = []
-
-		firststrand.each do |s|
-			if firststrand[i] == "U"
-				secondstrand.push("A")
-			elsif firststrand[i] == "G"
-				secondstrand.push("C")
-			elsif firststrand[i] == "A"
-				secondstrand.push("T")
-			elsif firststrand[i] == "C"
-				secondstrand.push("G")
-			else
-				puts "No"
-			end 
-		i += 1
-		end
-	secondstrand.map { |i| "" + i.to_s + "" }.join("")
-	
-	end  
-end 
-
-#Complement.of_dna('ACGTGGTCTTAA')
-
-
-
-
-
-
-
- 
-
-
-	# 	secondstrand.each do |i|
-	# 		if i == firststrand[startposition]
-		
-	# 		else
-	# 			array.push(1)	
-	# 		end 	
-	# 	startposition += 1
-	# 	end
-	# 	array.count
-	# end 
-
-
-
-
-
-
-
+  def self.key
+    {'G' => 'C', 'C' => 'G', 'T' => 'A', 'A' => 'U'}
+  end
+end
