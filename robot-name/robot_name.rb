9220cb3d1055c005ae295836ff232203aae71710
Bret Doucette <doucette.bret@gmail.com>
2015-08-02 10:47:40 -0600

    Removed white space

--- robot-name/robot_name.rb
+++ robot-name/robot_name.rb
@@ -1,34 +1,20 @@
 class Robot
 
-	def name
-		@name ||= "#{letter}#{number}"
-	end
+  def name
+    @name ||= "#{letter}#{number}"
+  end
 
-	def reset
-		@name = nil
-	end
+  def reset
+    @name = nil
+  end
 
-	def number
-		number = rand(100..999)
-		number.to_s
-	end
+  def number
+    number = rand(100..999)
+    number.to_s
+  end
 
-	def letter
-		alphabet = ("A".."Z").to_a.sample(2)
-		alphabet.join
-	end 	
+  def letter
+    alphabet = ("A".."Z").to_a.sample(2)
+    alphabet.join
+  end
 end
-
-robot = Robot.new
-puts robot.name
-puts robot.name
-robot.reset 
-puts robot.name
-
-
-
-
-
-
-
-
