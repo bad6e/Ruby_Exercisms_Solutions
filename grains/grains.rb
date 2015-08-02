2a998c01cd4dbcea99fe155f9d667168b7fd350a
Bret Doucette <doucette.bret@gmail.com>
2015-08-02 16:20:16 -0600

    Cleaned up whitespace

--- grains/grains.rb
+++ grains/grains.rb
@@ -1,26 +1,10 @@
 class Grains
 
-	def self.square(n)
-		2**(n - 1)
-  	end
+  def self.square(number)
+    (2) ** (number - 1)
+  end
 
-  	def self.total
-  		square(65) - 1 
-  		end 		
-
-
-
-
-
-		
-			
-
-			
-		  
-	
-end 
-
-puts Grains.square(64)
-puts Grains.total
-
-
+  def self.total
+    square(65)-1
+  end
+end
