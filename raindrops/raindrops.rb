a973bc7a290d5b6cb4c77004a9eb8a8395919177
Bret Doucette <doucette.bret@gmail.com>
2015-08-02 17:26:42 -0600

    Refactored Code

--- raindrops/raindrops.rb
+++ raindrops/raindrops.rb
@@ -1,35 +1,11 @@
 class Raindrops
-	VERSION = 1
+  VERSION = 2
 
-	def self.convert(number)
-		primenumber = Prime.prime_division(number).flat_map { |factor, power| [factor] * power }
-		puts prime = primenumber	
-		if primenumber.include? 7 and primenumber.include? 5 and primenumber.include? 3
-			puts "PlingPlangPlong"
-		elsif primenumber.include? 7 and primenumber.include? 3
-			puts "PlingPlong"
-		elsif primenumber.include? 5 and primenumber.include? 7
-			puts "PlangPlong"
-		elsif primenumber.include? 5 and primenumber.include? 3 
-			puts "PlingPlang"
-		elsif primenumber.include? 3
-			puts "Pling"
-		elsif primenumber.include? 5
-			puts "Plang"
-		elsif primenumber.include? 7
-			puts "Plong"
-		else
-			number.to_s
-		end 
-	end 
-end 
-
-	
-Raindrops.convert(1755)
-
-
-
-
-
-
-
+  def self.convert(number)
+    start = ""
+    start += 'Pling' if number % 3==0
+    start += 'Plang' if number % 5==0
+    start += 'Plong' if number % 7==0
+    start.empty? ? number.to_s : start
+  end
+end
