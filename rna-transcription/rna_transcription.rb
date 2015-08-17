6a3f7658353a2e275589d13566a11181d9eb0b83
Bret Doucette <doucette.bret@gmail.com>
2015-08-17 14:19:00 -0600

    Refactored code

--- rna-transcription/rna_transcription.rb
+++ rna-transcription/rna_transcription.rb
@@ -1,21 +1,22 @@
 class Complement
   VERSION = 2
+
+  def self.split_strand(strand)
+    strand.split('')
+  end
+
   def self.of_dna(strand)
-    incoming_strand = strand.split("")
-    raise ArgumentError, "WRONG" if incoming_strand == ["U"]
-    rna_strand = []
-    incoming_strand.map do |word|
-      rna_strand << key[word]
+    raise ArgumentError, "WRONG" if split_strand(strand)  == ["U"]
+    rna_strand = split_strand(strand).map do |letter|
+      key[letter]
     end
     rna_strand.join("")
   end
 
   def self.of_rna(strand)
-    incoming_strand = strand.split("")
-    raise ArgumentError, "WRONG" if incoming_strand == ["T"]
-    dna_strand = []
-    incoming_strand.map do |word|
-      dna_strand << key.invert[word]
+    raise ArgumentError, "WRONG" if split_strand(strand) == ["T"]
+    dna_strand = split_strand(strand).map do |letter|
+      key.invert[letter]
     end
     dna_strand.join("")
   end
