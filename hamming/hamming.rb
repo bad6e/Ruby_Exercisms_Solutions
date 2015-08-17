df9aa0e1e41be07192b39b0da04c2984fd25ff82
Bret Doucette <doucette.bret@gmail.com>
2015-08-17 15:37:27 -0600

    Refactored code

--- hamming/hamming.rb
+++ hamming/hamming.rb
@@ -1,16 +1,10 @@
 class Hamming
-  VERSION = 3
+  VERSION = 5
 
   def self.compute(strand_1, strand_2)
     raise ArgumentError, "The two strands must have the same length." if strand_1.length != strand_2.length
-    strand_1 = strand_1.split('')
-    strand_2 = strand_2.split('')
-    strand = strand_1.select.with_index do |letter, index|
-      letter != strand_2[index]
+    (0..strand_1.length).count do |position|
+      strand_1[position] != strand_2[position]
     end
-      strand.count
   end
 end
-
-
-
